bca6ab09335284b774ffb0fb496f9e7fa23548e7
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2025-10-01 21:08:40 -0400

    update allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ["localhost", "postedit.mitchk.hackclub.app", "api.postedit.mitchk.hackclub.app"]
+		allowedHosts: ["localhost", ".postedit.mitchk.hackclub.app"]
 	}
 });
